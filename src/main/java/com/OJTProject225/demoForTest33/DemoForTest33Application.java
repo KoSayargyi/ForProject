f03b6b8b1de091dc2d5a8041b2e2733f29ba91f7
first comment <swanhtetaung308@gmail.com>
2024-03-04 11:25:18 +0630

    first commit

---
+++ src/main/java/com/OJTProject225/demoForTest33/DemoForTest33Application.java
@@ -0,0 +1,13 @@
+package com.OJTProject225.demoForTest33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForTest33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForTest33Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/OJTProject225/demoForTest33/DemoForTest33ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OJTProject225.demoForTest33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForTest33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
